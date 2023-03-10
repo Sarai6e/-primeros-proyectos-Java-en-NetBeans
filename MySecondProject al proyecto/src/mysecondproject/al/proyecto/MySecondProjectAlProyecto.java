08a3e068eb9c64269bddff8c65c62431d2117dc5
Sarai6e <1472173@senati.pe>
2023-03-10 10:49:43 -0500

    Primer proyecto en Java

--- MySecondProject al proyecto/src/mysecondproject/al/proyecto/MySecondProjectAlProyecto.java
+++ MySecondProject al proyecto/src/mysecondproject/al/proyecto/MySecondProjectAlProyecto.java
@@ -1,12 +1,14 @@
-package creacion;
-public class Creacion {
+
+package mysecondproject.al.proyecto;
+
+public class MySecondProjectAlProyecto {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello Mundo world");
+        System.out.println("Hello again world");
     }
     
 }
